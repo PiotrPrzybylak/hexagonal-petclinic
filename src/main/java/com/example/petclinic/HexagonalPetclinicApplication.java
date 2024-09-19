3700cfd65b7fc6a896dee19e2c7c8d7e9eea046c
Piotr Przybylak <piotr.przybylak@gmail.com>
2024-09-19 16:55:46 +0200

    Fix packaging

--- src/main/java/com/example/petclinic/HexagonalPetclinicApplication.java
+++ src/main/java/com/example/petclinic/HexagonalPetclinicApplication.java
@@ -1,4 +1,4 @@
-package com.example.hexagonal_petclinic;
+package com.example.petclinic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
