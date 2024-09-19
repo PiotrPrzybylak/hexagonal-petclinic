3405f0ca028d7694e7c3e0b89d0167159196d144
Piotr Przybylak <piotr.przybylak@gmail.com>
2024-09-19 18:57:03 +0200

    Add REST endpoint for ListProductsUseCase

---
+++ src/main/java/com/example/petclinic/infrastructure/incoming/rest/ListProductsEndpoint.java
@@ -0,0 +1,16 @@
+package com.example.petclinic.infrastructure.incoming.rest;
+
+import com.example.petclinic.ports.incoming.ListProductsUseCase;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+record ListProductsEndpoint(ListProductsUseCase useCase) {
+
+    @GetMapping("/api/products")
+    public List<ListProductsUseCase.Product> get() {
+        return  useCase.list();
+    }
+}
