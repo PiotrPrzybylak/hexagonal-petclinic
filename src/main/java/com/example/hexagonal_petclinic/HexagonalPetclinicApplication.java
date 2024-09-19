84d79cadcbbb4fd03bde5201cf0785027515f67b
Piotr Przybylak <piotr.przybylak@gmail.com>
2024-09-19 16:24:33 +0200

    Generate project from start.spring.io

---
+++ src/main/java/com/example/hexagonal_petclinic/HexagonalPetclinicApplication.java
@@ -0,0 +1,13 @@
+package com.example.hexagonal_petclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexagonalPetclinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HexagonalPetclinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hexagonal_petclinic/HexagonalPetclinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hexagonal_petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexagonalPetclinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
